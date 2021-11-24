6cdba92e26b24666eecb6556d304cf70066f1c15
Kristina <kristinakocur2@gmail.com>
2021-11-24 20:14:16 +0100

    Integrated Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,19 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
     const [word, setWord] = useState("");
+    function handleResponse(reponse) {
+        console.log(reponse);
+    }
+    
     function search(event) {
         event.preventDefault();
-        alert(`Searching for: ${word}`);
-
+        
+        let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+        axios.get(apiURL).then(handleResponse);
     }
 
     function handleSearch(event) {
